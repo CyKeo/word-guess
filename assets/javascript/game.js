01dd4c8a2937112d8b9d546e7d079de8e1d27715
Frosty <alexis@alexiss-mbp.uguest.utah.edu>
2018-08-13 21:08:22 -0600

    some things

---
+++ assets/javascript/game.js
@@ -0,0 +1,10 @@
+console.log("hello")
+
+var randWords = ("jason vorhees", "freddy kruger", "scary", "spooky","zombie","ghost");
+
+var guessesLeft = 8;
+
+var guessedLetters = [];
+
+var wins = 0;
+
